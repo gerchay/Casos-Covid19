1125ab4442d07f7e13e20cc0e738c5c76bdaf0c0
Luis Vargas <3005139410101@ingenieria.usac.edu.gt>
2020-12-29 00:41:07 -0600

    Publish with golang

---
+++ redispub/go.mod
@@ -0,0 +1,8 @@
+module github.com/luislvar/proyecto2_grupo5_so1
+
+go 1.15
+
+require (
+	github.com/go-redis/redis/v8 v8.4.4
+	github.com/gorilla/mux v1.8.0
+)
--- redispub/publisher.go
+++ redispub/publisher.go
@@ -1,11 +1,82 @@
 package main
 
 import (
-
+"fmt"
+"log"
+"net/http"
+"encoding/json"
+"github.com/gorilla/mux"
+"github.com/go-redis/redis/v8"
 )
+// ------------------------------------
+
+type Post struct {
+	Name string `json:"name"`
+	Location string `json:"location"`
+	Age json.Number `json:"age"`
+	Infected_type string `json:"infected_type"`
+	State string `json:"state"`
+  }
+
+
+func publish(w http.ResponseWriter, r *http.Request ){
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "35.238.130.224:6379",
+		Password: "redissopes1",
+		DB: 	0,
+		})
+	defer client.Close()
+
+
+	err := client.Publish(r.Context(), "canal1", "Conectandome desde golang papu").Err()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func publishJSON(w http.ResponseWriter, r *http.Request ){
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "35.238.130.224:6379",
+		Password: "redissopes1",
+		DB: 	0,
+		})
+	defer client.Close()
+
+	w.Header().Set("Content-Type", "application/json")
+	
+	var post Post 
+
+	_ = json.NewDecoder(r.Body).Decode(&post)
+
+	fmt.Println(post)
+	fmt.Println(post.Name)
+	fmt.Println(post.Age)
+	json.NewEncoder(w).Encode(&post)
+	// err := client.Publish(r.Context(), "canal1", r.Body).Err()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+}
+
+func homePage( w http.ResponseWriter, r *http.Request ){
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRequests(){
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/publish", publish).Methods("GET")
+	myRouter.HandleFunc("/publish/json", publishJSON).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
 
 func main(){
-
-
-	
+	handleRequests()
 }
