cd201cd60d9a616290c47a50012842a98dcb3b3b
Luis Vargas <3005139410101@ingenieria.usac.edu.gt>
2021-01-05 17:25:21 -0600

    Serverless function

---
+++ serverless/main.py
@@ -0,0 +1,17 @@
+import redis
+import json
+
+def hello_world():
+    pool = redis.ConnectionPool(host="35.238.130.224", port=6379\
+    ,password="redissopes1",db=0,decode_responses=True)
+
+    r = redis.Redis(connection_pool=pool)
+
+    val = r.get("a")
+    lista = r.lrange("listaCasos", 0, -1)
+    lista2 = ['2', '1']
+    jsonlist = json.dumps(lista)
+    print(jsonlist)
+    print(val)
+
+hello_world()
